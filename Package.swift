9e3b31fadf59bee5b40701e5d8cc32f2a41ab507
TraxxasWyls <savinov.dsta@gmail.com>
2023-11-07 20:56:09 +0300

    Fix resources

--- Package.swift
+++ Package.swift
@@ -18,7 +18,11 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Toast"),
+            name: "Toast",
+            resources: [
+                .copy("Resources/Images/ToastAssets.xcassets")
+            ]
+        ),
         .testTarget(
             name: "ToastTests",
             dependencies: ["Toast"]),
